7a49a0def3cd7748afe5defbec4b83a8d99dd749
Manya Tuli <72210577+manya0702@users.noreply.github.com>
2024-09-06 00:04:34 +0530

    Create RectangleTest.java

---
+++ RectangleTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RectangleTest {
+
+    @Test
+    public void testCalculateArea() {
+        // Test case 1: length = 5.0, width = 3.0
+        Rectangle rect1 = new Rectangle(5.0, 3.0);
+        assertEquals(15.0, rect1.calculateArea(), 0.001);
+
+        // Test case 2: length = 7.5, width = 4.0
+        Rectangle rect2 = new Rectangle(7.5, 4.0);
+        assertEquals(30.0, rect2.calculateArea(), 0.001);
+
+        // Test case 3: length = 0.0, width = 0.0
+        Rectangle rect3 = new Rectangle(0.0, 0.0);
+        assertEquals(0.0, rect3.calculateArea(), 0.001);
+
+        // Test case 4: length = -5.0, width = 3.0 (negative value test)
+        Rectangle rect4 = new Rectangle(-5.0, 3.0);
+        assertEquals(-15.0, rect4.calculateArea(), 0.001);
+    }
+}
